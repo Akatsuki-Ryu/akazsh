561df013425ff79300e4f19791225d69eb321c28
akatsuki <akatsuki@akamacair.local>
2016-06-13 22:02:18 +0300

    temp

---
+++ adb.sh
@@ -0,0 +1,12 @@
+cd /Library/Application\ Support/Adobe
+rm -r SLCache
+rm -r SLStore
+cd ~/Library/Application\ Support/Adobe/
+rm -r OOBE
+cd ~/Library/Saved\ Application\ State
+rm -r com.adobe*
+cd /Library/Caches/
+rm -r .*
+cd ~/Library/Preferences/Adobe
+rm -r .*
+
