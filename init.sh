ea20e29baf4ec1f6146e9db432f2d95c6e57838c
akatsuki <AkafreakingBox>
2019-02-04 23:16:59 +0200

    adding the tigrc file init version

--- init.sh
+++ init.sh
@@ -4,10 +4,17 @@ echo this will overwrite the setting on this user ....
 read -p "ok? (y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 	
+rm -r .gitconfig
+rm -r .config	
+rm -r .tmux.conf
+rm -r .zshrc
+rm -r .ssh	
+rm -r tigrc
+
 ln -s akazsh/.gitconfig .gitconfig
  ln -s akazsh/.config .config
   ln -s akazsh/.tmux.conf.local .tmux.conf.local
   ln -s akazsh/.tmux.conf .tmux.conf
   ln -s akazsh/.zshrc .zshrc
   ln -s akazsh/.ssh .ssh
-
+  ln -s akazsh/.tigrc .tigrc
