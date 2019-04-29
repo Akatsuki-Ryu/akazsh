3e206c16d3ca4b830df7792a88e519e6145f3ed0
AkaboxAvi <AkaFreakingBox>
2019-04-30 02:36:35 +0300

    update gitconfig fire to local enviroment

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-echo now we will install brew . press N to skip 
+#!/usr/bin/env bash
+echo now we will install brew . press N to skip
 
 read -p "ok? (y/N): " yn
 case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -12,21 +13,23 @@ case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubuserconten
 
 
 cd ..
-echo this will overwrite the setting on this user .... 
+echo this will overwrite the setting on this user ....
 
 read -p "ok? (y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 
-# linking diff_highlight to system . git should be from brew . this needs to be confirmed 
+# linking diff_highlight to system . git should be from brew . this needs to be confirmed
 	sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
-	
-rm -r .gitconfig
-rm -r .config	
+
+# taking gitconfig to seperate machines.
+#rm -r .gitconfig
+rm -r .config
 rm -r .tmux.conf
 rm -r .zshrc
 rm -r .tigrc
 
-ln -s akazsh/.gitconfig .gitconfig
+#ln -s akazsh/.gitconfig .gitconfig
+cp akazsh/.gitconfig .gitconfig
  ln -s akazsh/.config .config
   ln -s akazsh/.tmux.conf.local .tmux.conf.local
   ln -s akazsh/.tmux.conf .tmux.conf
