edfba6f80156509f82c0b481cb6b48ef67fc5463
AkaboxDigi <akafreakingbox>
2019-08-27 18:05:39 +0300

    update init

--- init.sh
+++ init.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
-echo now we will install brew . press N to skip
+echo now we will install basic apps , N to skip
 
 read -p "ok? (y/N): " yn
-case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew install rcmdnk/file/brew-file
-	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-;; *) echo "skip." ; ;; esac
+case "$yn" in [yY]*) #this is the condition yes 
+
+#installing oh my zsh and powerlevel9k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+#install font for powerlevel
+sudo apt-get install fonts-powerline
+
+#install some basic features  
+sudo apt-get install autojump
+sudo apt-get install tig
+
+# linking diff_highlight to system . git should be from brew . this needs to be confirmed
+	sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+
+
+
+
+;; *)  #this is the condition no 
+ echo "skip." ; ;; esac
 
 
 
@@ -18,22 +35,12 @@ cd ..
 echo this will overwrite the setting on this user ....
 
 read -p "ok? (y/N): " yn
-case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+case "$yn" in [yY]*)  #this is condition for yes
 
-# linking diff_highlight to system . git should be from brew . this needs to be confirmed
-	sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+	
 
 
-#installing oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-#install font for powerlevel
-sudo apt-get install fonts-powerline
-
-#install some basic features  
-sudo apt-get install autojump
-sudo apt-get install tig
 
 
 # taking gitconfig to seperate machines.
@@ -53,3 +60,7 @@ cp akazsh/.gitconfig .gitconfig
   ln -s akazsh/.tigrc .tigrc
 
 
+
+	;; *) #this is condition for no 
+
+echo "abort." ; exit ;; esac
