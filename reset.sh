d880815e914ec41e25949038afb405e4a49b3230
AkaboxAvi <AkaFreakingBox>
2019-12-30 17:48:38 +0200

    script remove all brew packages

--- reset.sh
+++ reset.sh
@@ -3,6 +3,7 @@ echo now we will UNinstall brew and reset. press N to skip
 
 read -p "ok? (y/N): " yn
 case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+	brew remove --force $(brew list) --ignore-dependencies  
 
 ;; *) echo "skip." ; ;; esac
 
@@ -18,15 +19,16 @@ rm -r .gitconfig
 rm -r .config
 rm -r .tmux.conf
 rm -r .tmux.conf.local
-rm -r .zshrc
+rm -r .zshrc 
 rm -r .tigrc
+
 
 #ln -s akazsh/.gitconfig .gitconfig
 cp akazsh/.gitconfig .gitconfig
- ln -s akazsh/.config .config
-  ln -s akazsh/tmux/.tmux.conf.local .tmux.conf.local
-  ln -s akazsh/tmux/.tmux.conf .tmux.conf
-  ln -s akazsh/.zshrc .zshrc
-  ln -s akazsh/.tigrc .tigrc
+ln -s akazsh/.config .config
+ln -s akazsh/tmux/.tmux.conf.local .tmux.conf.local
+ln -s akazsh/tmux/.tmux.conf .tmux.conf
+ln -s akazsh/.zshrc .zshrc
+ln -s akazsh/.tigrc .tigrc
 
 
