699a19df207881c8045b30b510239de0a5467e7b
AkaboxAvi <AkaFreakingBox>
2019-12-31 19:46:23 +0200

    brew init script updates

--- misc/brewinit.sh
+++ misc/brewinit.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+# from https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
 
 # Install command-line tools using Homebrew.
+
+
+which -s brew
+if [[ $? != 0 ]] ; then
+  yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew already installed ..."
+fi
+
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -23,14 +33,39 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+# brew install bash
+# brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
 #if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
 #  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
 #  chsh -s "${BREW_PREFIX}/bin/bash";
 #fi;
+
+# Install zsh
+brew install zsh
+brew install tree
+# brew install getantibody/tap/antibody
+
+
+
+# Check if zsh is in list of accepted shells
+if grep -Fxq "/usr/local/bin/zsh" /etc/shells > /dev/null 2>&1; then
+  echo "zsh is already in the list of accepted shells ..."
+else
+  # If not found
+  echo "Adding zsh to list of accepted shells ..."
+  sudo sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
+fi
+
+# Check if zsh is default shell
+if echo $SHELL | grep /bin/bash > /dev/null 2>&1; then
+  echo "Setting zsh as default shell ..."
+  chsh -s /usr/local/bin/zsh
+else
+  echo "zsh is already the default shell ..."
+fi
+
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -42,7 +77,7 @@ brew install gnupg
 brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
-brew install screen
+# brew install screen
 brew install php
 brew install gmp
 
@@ -78,12 +113,20 @@ brew install ucspi-tcp # `tcpserver` etc.
 brew install xpdf
 brew install xz
 
+
+# Git
+brew install git
+brew install git-flow
+brew install git-lfs
+# brew install cdiff
+brew install hub
+
 # Install other useful binaries.
-brew install ack
+#brew install ack
 #brew install exiv2
 brew install git
-brew install git-lfs
-brew install gs
+
+#brew install gs
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
@@ -93,9 +136,34 @@ brew install pv
 brew install rename
 brew install rlwrap
 brew install ssh-copy-id
-brew install tree
 brew install vbindiff
 brew install zopfli
+
+
+# Development
+brew install python
+brew install python3
+brew install ruby
+
+# Highlighting
+brew install source-highlight
+
+
+
+
+# font related (temp)
+brew cask install homebrew/cask-fonts/font-hack-nerd-font
+
+
+# Other
+brew install mas
+brew install neovim
+brew install wget
+# brew install gdrive
+brew install micro
+
+
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
---
+++ misc/caskinit.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# Browsers
+brew cask install google-chrome-canary
+
+# Development
+brew cask install visual-studio-code
+brew cask install iterm2
+
+# Communication
+brew cask install whatsapp
+
+# Cloud
+brew cask install dropbox
+brew cask install google-backup-and-sync
+
+# Productivity
+brew cask install alfred
+
+# Media
+
+
+# Mac OS Enhancements
+brew cask install the-unarchiver
+brew tap buo/cask-upgrade
+
+# Other
+brew cask install font-hack-nerd-font
+
+# Install Mac App Store apps
+# add Magnet install
