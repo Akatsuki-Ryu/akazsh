ab16ce69fad4d8321b09944218ce1668f5770d10
AkaboxAvi <AkaFreakingBox>
2019-12-31 20:11:56 +0200

    update caskinit to mine

--- misc/caskinit.sh
+++ misc/caskinit.sh
@@ -5,22 +5,51 @@ brew tap caskroom/versions
 
 # Browsers
 brew cask install google-chrome-canary
+brew cask install google-chrome
+brew cask install tor-browser
+brew cask install zeronet
+
 
 # Development
-brew cask install visual-studio-code
+# brew cask install visual-studio-code
 brew cask install iterm2
+brew cask install gitkraken
+brew cask install intellij-idea
+brew cask install java
+brew cask install plistedit-pro
+brew cask install sublime-text
+brew cask install wireshark
+
+
+
+
+
 
 # Communication
-brew cask install whatsapp
+# brew cask install whatsapp
+brew cask install franz
+brew cask install slack
+
+
 
 # Cloud
-brew cask install dropbox
+# brew cask install dropbox
 brew cask install google-backup-and-sync
 
 # Productivity
-brew cask install alfred
+# brew cask install alfred
+brew cask install docker
+brew cask install energybar
+brew cask install grammarly
+brew cask install kitematic
+brew cask install rescuetime
+
+
+
 
 # Media
+brew cask install blender
+brew cask install soundflower
 
 
 # Mac OS Enhancements
@@ -32,3 +61,34 @@ brew cask install font-hack-nerd-font
 
 # Install Mac App Store apps
 # add Magnet install
+
+
+
+#!/bin/sh
+
+# brew cask install WhatsApp
+# brew cask install Itsycal
+# brew cask install Kindle
+# brew cask install OneDrive
+# brew cask install Rocket
+# brew cask install Transmission
+# brew cask install Alfred
+# brew cask install Google-Chrome
+# brew cask install iTerm2
+# brew cask install vs-code
+# brew cask install hyperswitch
+# brew cask install swinsian
+brew cask install syncthing
+# brew cask install vlc
+# brew cask install iina
+# brew cask install GrandPerspective
+# brew cask install Pinta
+brew cask install Amphetamine
+brew cask install Djview
+# brew cask install self-control
+# brew cask install the-unarchiver
+
+
+# disk management 
+brew cask install daisydisk
+
