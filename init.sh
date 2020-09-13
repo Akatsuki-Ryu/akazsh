9912878404381f3b56b00e829f95c293ae619426
Akabox <Akabox>
2020-09-13 23:59:49 +0300

    update ohmyzsh install source

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubuserconten
 
 	brew install rcmdnk/file/brew-file
 	brew install sambadevi/powerlevel9k/powerlevel9k
-	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	read -p "install brew apps and cask apps ,ok? (y/N): " yn
 	case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	 ./misc/brewinit.sh
