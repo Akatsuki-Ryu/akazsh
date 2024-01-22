0dd3ff6b2ad0abee2b8708ce0c72b04de9ead5ff
Akabox <Akabox@akabox.com>
2024-01-22 17:20:11 +0200

    add disable spotlight

---
+++ misc/disablespotlight.sh
@@ -0,0 +1,5 @@
+#this will disable the spotlight indexing to prevent mdworker shared kernel panic situation 
+
+sudo mdutil -a -i off
+
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
