7ad0c601eb7f08423006178670b2979d932f2c02
Akabox <Akabox@akabox.com>
2024-01-22 15:52:39 +0200

    add change name and password policy

--- misc/changename.sh
+++ misc/changename.sh
@@ -4,3 +4,7 @@ read computername
 sudo scutil --set ComputerName "$computername"
 sudo scutil --set LocalHostName "$computername"
 sudo scutil --set HostName "$computername"
+
+echo change password policy
+sudo pwpolicy setaccountpolicies passwdpolicy.xml
+passwd
