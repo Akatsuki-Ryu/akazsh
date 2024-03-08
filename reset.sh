2aa02676c8847dcfb87545e14fcb5739e728f3bb
AkaboxCyberMac <AkaboxCyberMac@aka.com>
2024-03-08 12:23:28 +0200

    update reset script for mytmux

--- reset.sh
+++ reset.sh
@@ -2,34 +2,36 @@
 echo now we will UNinstall brew and reset. press N to skip
 
 read -p "ok? (y/N): " yn
-case "$yn" in [yY]*) /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-	brew remove --force $(brew list) --ignore-dependencies  
+case "$yn" in [yY]*)
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+	brew remove --force $(brew list) --ignore-dependencies
 	brew cask remove --force $(brew cask list)
 
-;; *) echo "skip." ; ;; esac
-
+	;;
+*) echo "skip." ;; esac
 
 cd ..
 echo this will overwrite the setting on this user ....
 
 read -p "ok? (y/N): " yn
-case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+case "$yn" in [yY]*) ;; *)
+	echo "abort."
+	exit
+	;;
+esac
 
 # taking gitconfig to seperate machines.
 rm -r .gitconfig
 rm -r .config
 rm -r .tmux.conf
 rm -r .tmux.conf.local
-rm -r .zshrc 
+rm -r .zshrc
 rm -r .tigrc
-
 
 #ln -s akazsh/.gitconfig .gitconfig
 cp akazsh/.gitconfig .gitconfig
 ln -s akazsh/.config .config
-ln -s akazsh/tmux/.tmux.conf.local .tmux.conf.local
-ln -s akazsh/tmux/.tmux.conf .tmux.conf
+ln -s akazsh/mytmux/.tmux.conf.local .tmux.conf.local
+ln -s akazsh/mytmux/.tmux.conf .tmux.conf
 ln -s akazsh/.zshrc .zshrc
 ln -s akazsh/.tigrc .tigrc
-
-
