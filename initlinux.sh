95b70e31e586470d404acc2f9ed22a061c8b8efc
Akafreakingbox <akafreakingbox>
2024-03-08 23:50:48 +0200

    fmt

--- initlinux.sh
+++ initlinux.sh
@@ -8,27 +8,25 @@ case "$yn" in [yY]*) #this is the condition yes
   sudo apt-get upgrade -y
   sudo apt-get install -y git
 
-	#install basic system components
-	sudo apt-get install -y zsh
-	sudo apt-get install -y curl
+  #install basic system components
+  sudo apt-get install -y zsh
+  sudo apt-get install -y curl
 
-	#installing oh my zsh and powerlevel9k
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-	#installing zsh plugins
-	# git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+  #installing oh my zsh and powerlevel9k
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  #git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+  #installing zsh plugins
+  # git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
+  # linking diff_highlight to system . git should be from brew . this needs to be confirmed
+  sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
 
-
-	# linking diff_highlight to system . git should be from brew . this needs to be confirmed
-	sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
-
-	;;
+  ;;
 *) #this is the condition no
-	echo "skip." ;;
+  echo "skip." ;;
 esac
 
 # exit
@@ -39,28 +37,28 @@ echo this will overwrite the setting on this user ....
 read -p "ok? (y/N): " yn
 case "$yn" in [yY]*) #this is condition for yes
 
-	# taking gitconfig to seperate machines.
-	rm -r .gitconfig
-	rm -r .config
-	rm -r .tmux.conf
-	rm -r .tmux.conf.local
-	rm -r .zshrc
-	rm -r .tigrc
-	rm -r .p10k.zsh
+  # taking gitconfig to seperate machines.
+  rm -r .gitconfig
+  rm -r .config
+  rm -r .tmux.conf
+  rm -r .tmux.conf.local
+  rm -r .zshrc
+  rm -r .tigrc
+  rm -r .p10k.zsh
 
-	#ln -s akazsh/.gitconfig .gitconfig
-	cp akazsh/.gitconfig .gitconfig
-	ln -s akazsh/.config .config
-	ln -s akazsh/mytmux/.tmux.conf.local .tmux.conf.local
-	ln -s akazsh/mytmux/.tmux.conf .tmux.conf
-	ln -s akazsh/.zshrc .zshrc
-	ln -s akazsh/.tigrc .tigrc
-	ln -s akazsh/.p10k.zsh .p10k.zsh
+  #ln -s akazsh/.gitconfig .gitconfig
+  cp akazsh/.gitconfig .gitconfig
+  ln -s akazsh/.config .config
+  ln -s akazsh/mytmux/.tmux.conf.local .tmux.conf.local
+  ln -s akazsh/mytmux/.tmux.conf .tmux.conf
+  ln -s akazsh/.zshrc .zshrc
+  ln -s akazsh/.tigrc .tigrc
+  ln -s akazsh/.p10k.zsh .p10k.zsh
 
-	;;
+  ;;
 *) #this is condition for no
 
-	echo "abort."
-	exit
-	;;
+  echo "abort."
+  exit
+  ;;
 esac
--- misc/aptinit.sh
+++ misc/aptinit.sh
@@ -1,38 +1,38 @@
-	#install font for powerlevel
-	sudo apt-get install -y fonts-powerline
+#install font for powerlevel
+sudo apt-get install -y fonts-powerline
 
-	#install some basic features
-	sudo apt-get install -y autojump
-	sudo apt-get install -y tig
-	# sudo apt-get install -y terminator
-	sudo apt-get install -y mc
-	sudo apt-get install -y ncdu
-	sudo apt-get install -y micro
-	sudo apt-get install -y prettyping
-	#sudo apt-get install -y bat
-	sudo apt-get install -y duf #better df
-	#sudo apt-get install -y exa	#better ls , this is deprecated
-	./misc/installeza.sh
-	sudo apt-get install htop
-	sudo apt-get install ripgrep
+#install some basic features
+sudo apt-get install -y autojump
+sudo apt-get install -y tig
+# sudo apt-get install -y terminator
+sudo apt-get install -y mc
+sudo apt-get install -y ncdu
+sudo apt-get install -y micro
+sudo apt-get install -y prettyping
+#sudo apt-get install -y bat
+sudo apt-get install -y duf #better df
+#sudo apt-get install -y exa	#better ls , this is deprecated
+./misc/installeza.sh
+sudo apt-get install htop
+sudo apt-get install ripgrep
 
-	#install docker
-	sudo apt install docker-compose -y
-	sudo groupadd docker
-	sudo gpasswd -a $USER docker
-	newgrp docker
+#install docker
+sudo apt install docker-compose -y
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
 
-	#install dry ( docker manager in terminal)
-	curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
-	sudo chmod 755 /usr/local/bin/dry
+#install dry ( docker manager in terminal)
+curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
+sudo chmod 755 /usr/local/bin/dry
 
-	#install nvm
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+#install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-	#install tailscale
-	curl -fsSL https://tailscale.com/install.sh | sh
+#install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
 
-	#network related
-	sudo apt-get install -y samba
-	sudo apt-get install -y netatalk
-	sudo cp misc/afplinux.conf /etc/netatalk/afp.conf
+#network related
+sudo apt-get install -y samba
+sudo apt-get install -y netatalk
+sudo cp misc/afplinux.conf /etc/netatalk/afp.conf
