82aa149a1e5e09dc16954097c9af187c59ed4738
AkaboxCyberMac <AkaboxCyberMac@aka.com>
2024-03-09 02:40:44 +0200

    remove bak when reset the nvim

--- misc/resetnvim.sh
+++ misc/resetnvim.sh
@@ -1,6 +1,11 @@
 # required
 # mv ~/.config/nvim{,.bak}
 
+# delete the bak folders
+rm -r ~/.local/share/nvim.bak
+rm -r ~/.local/state/nvim.bak
+rm -r ~/.cache/nvim.bak
+
 # optional but recommended
 mv ~/.local/share/nvim{,.bak}
 mv ~/.local/state/nvim{,.bak}
