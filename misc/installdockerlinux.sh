7788c2e71911a6d1fd1f75ef8a898446d8c7d339
AkaboxLinux <AkaboxLinux@aka.com>
2024-03-11 01:52:39 +0200

    install docker cilently

--- misc/installdockerlinux.sh
+++ misc/installdockerlinux.sh
@@ -10,7 +10,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 # 	gnupg-agent \
 # 	software-properties-common
 #
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 # install gpg key
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -29,7 +29,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 #do update again
 sudo apt update
 
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 # cat /etc/apt/sources.list | grep docker
 # deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable
