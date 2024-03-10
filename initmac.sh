d0413080ff96876a5c8713bddc0e019946205863
Akafreakingbox <akafreakingbox>
2024-03-11 00:11:30 +0200

    fmt

--- initmac.sh
+++ initmac.sh
@@ -7,14 +7,18 @@ case "$yn" in [yY]*)
 
 	brew install rcmdnk/file/brew-file
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 	# brew install sambadevi/powerlevel9k/powerlevel9k
 	brew install powerlevel10k
+
 	#clean up the powerlevel10k folder if exists
 	rm -rf "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+
 	#clean up zsh plugins if folder exists
 	rm -rf "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 	rm -rf "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 	rm -rf "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}"/plugins/zsh-completions
+
 	#installing zsh plugins
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
 	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
