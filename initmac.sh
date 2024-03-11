15fc4e9335b0d26b79c70c992fc512d66efd57df
AkaboxLinux <AkaboxLinux@aka.com>
2024-03-12 00:17:44 +0200

    adjust zsh completion installation paramters

--- initmac.sh
+++ initmac.sh
@@ -23,8 +23,9 @@ case "$yn" in [yY]*)
 	#installing zsh plugins
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME"/.oh-my-zsh/custom/themes/powerlevel10k
 	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-~/.oh-my-zsh}"/custom/plugins/zsh-completions
+	# git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-~/.oh-my-zsh}"/custom/plugins/zsh-completions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}"/plugins/zsh-completions
 
 	read -rp "install brew apps and cask apps ,ok? (y/N): " yn
 	case "$yn" in [yY]*)
