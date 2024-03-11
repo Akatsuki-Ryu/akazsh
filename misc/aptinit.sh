74b51176ba1d32be283b9838ebed71b766dd2aa9
AkaboxLinux <AkaboxLinux@aka.com>
2024-03-11 02:12:51 +0200

    minor fix for aptinit.sh

--- misc/aptinit.sh
+++ misc/aptinit.sh
@@ -32,7 +32,7 @@ sudo apt-get install htop
 #install docker
 sudo apt install docker-compose -y
 sudo groupadd docker
-sudo gpasswd -a $USER docker
+sudo gpasswd -a "$USER" docker
 newgrp docker
 
 #install tmux
