eb35c38fa71a40eff683de0dde485dae12ff1f93
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-03-24 23:41:03 +0200

    change copilot accept keymap to control j

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,30 @@
+-- ~/.config/nvim/lua/plugins/copilot.lua
+-- copilot configuration
+return {
+    "zbirenbaum/copilot.lua",
+
+    opts = {
+        suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<C-j>" } },
+        panel = {
+            enabled = true,
+            auto_refresh = false,
+            keymap = {
+                jump_prev = "[[",
+                jump_next = "]]",
+                accept = "<CR>",
+                refresh = "gr",
+                open = "<M-CR>",
+            },
+            layout = {
+                position = "bottom", -- | top | left | right
+                ratio = 0.4,
+            },
+        },
+        filetypes = {
+            markdown = true,
+            help = true,
+        },
+    },
+}
+-- For additional configurations for the copilot panel, suggestions, filetypes supported, etc
+-- see https://github.com/zbirenbaum/copilot.lua
