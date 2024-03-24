f08233e0f36381428bded2b2522da46188178dd3
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-03-24 20:25:09 +0200

    add gen.nvim , also add option for offloading to my pc

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -39,8 +39,45 @@ require("lazy").setup({
         },
 
         -- Minimal configuration for local ai plugins , but we will use both for now
-        { "David-Kunz/gen.nvim" },
+        -- { "David-Kunz/gen.nvim" },
 
+        -- Custom Parameters (with defaults)
+        {
+            "David-Kunz/gen.nvim",
+            opts = {
+                host = "akaboxpc",
+            },
+            -- opts = {
+            --     model = "mistral", -- The default model to use.
+            --     host = "localhost", -- The host running the Ollama service.
+            --     -- host = "akaboxpc", -- The host running the Ollama service.
+            --     port = "11434", -- The port on which the Ollama service is listening.
+            --     quit_map = "q", -- set keymap for close the response window
+            --     retry_map = "<c-r>", -- set keymap to re-send the current prompt
+            --     init = function(options)
+            --         pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+            --     end,
+            --     -- Function to initialize Ollama
+            --     command = function(options)
+            --         local body = { model = opts.model, stream = true }
+            --         return "curl --silent --no-buffer -X POST http://"
+            --             .. options.host
+            --             .. ":"
+            --             .. options.port
+            --             .. "/api/chat -d $body"
+            --     end,
+            --     -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+            --     -- This can also be a command string.
+            --     -- The executed command must return a JSON object with { response, context }
+            --     -- (context property is optional).
+            --     -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+            --     display_mode = "float", -- The display mode. Can be "float" or "split".
+            --     show_prompt = false, -- Shows the prompt submitted to Ollama.
+            --     show_model = false, -- Displays which model you are using at the beginning of your chat session.
+            --     no_auto_close = false, -- Never closes the window automatically.
+            --     debug = false, -- Prints errors and the command which is run.
+            -- },
+        },
         -- import any extras modules here
         -- { import = "lazyvim.plugins.extras.linting.eslint" },
         { import = "lazyvim.plugins.extras.formatting.prettier" },
