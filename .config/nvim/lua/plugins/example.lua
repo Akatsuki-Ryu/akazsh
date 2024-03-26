a37c2224855aad1db0f7ff19303b1111f5d11a52
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-03-26 14:28:29 +0200

    remove duplicated lualine plugin

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -188,13 +188,13 @@ return {
 
     -- or you can return new options to override all the defaults
     {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = function()
-            return {
-                --[[add your custom lualine config here]]
-            }
-        end,
+        -- "nvim-lualine/lualine.nvim",
+        -- event = "VeryLazy",
+        -- opts = function()
+        --     return {
+        --         --[[add your custom lualine config here]]
+        --     }
+        -- end,
     },
 
     -- use mini.starter instead of alpha
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -91,14 +91,14 @@ return {
 
     -- statusline
     {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-            options = {
-                -- globalstatus = false,
-                theme = "solarized_dark",
-            },
-        },
+        -- "nvim-lualine/lualine.nvim",
+        -- event = "VeryLazy",
+        -- opts = {
+        --     options = {
+        --         -- globalstatus = false,
+        --         theme = "solarized_dark",
+        --     },
+        -- },
     },
 
     -- filename
