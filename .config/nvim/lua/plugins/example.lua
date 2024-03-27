40177d7895cf4c4c0398dc89edba6a10740d93b4
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-03-27 20:01:09 +0200

    fix treesitter problem

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -138,44 +138,44 @@ return {
 
     -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
     -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    --{ import = "lazyvim.plugins.extras.lang.typescript" },
 
-    -- add more treesitter parsers
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-        },
-    },
+    ---- add more treesitter parsers
+    --{
+    --    "nvim-treesitter/nvim-treesitter",
+    --    opts = {
+    --        ensure_installed = {
+    --            "bash",
+    --            "html",
+    --            "javascript",
+    --            "json",
+    --            "lua",
+    --            "markdown",
+    --            "markdown_inline",
+    --            "python",
+    --            "query",
+    --            "regex",
+    --            "tsx",
+    --            "typescript",
+    --            "vim",
+    --            "yaml",
+    --        },
+    --    },
+    --},
 
     -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
     -- would overwrite `ensure_installed` with the new value.
     -- If you'd rather extend the default config, use the code below instead:
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            -- add tsx and treesitter
-            vim.list_extend(opts.ensure_installed, {
-                "tsx",
-                "typescript",
-            })
-        end,
-    },
+    --{
+    --    "nvim-treesitter/nvim-treesitter",
+    --    opts = function(_, opts)
+    --        -- add tsx and treesitter
+    --        vim.list_extend(opts.ensure_installed, {
+    --            "tsx",
+    --            "typescript",
+    --        })
+    --    end,
+    --},
 
     -- the opts function can also be used to change the default opts:
     {
@@ -201,7 +201,7 @@ return {
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
     -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-    { import = "lazyvim.plugins.extras.lang.json" },
+    --{ import = "lazyvim.plugins.extras.lang.json" },
 
     -- add any tools you want to have installed below
     {
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,67 +1,95 @@
--- return {
---     { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
---
---     {
---         "nvim-treesitter/nvim-treesitter",
---         opts = {
---             ensure_installed = {
---                 "astro",
---                 "cmake",
---                 "cpp",
---                 "css",
---                 "fish",
---                 "gitignore",
---                 "go",
---                 "graphql",
---                 "http",
---                 "java",
---                 "php",
---                 "rust",
---                 "scss",
---                 "sql",
---                 "svelte",
---             },
---
---             -- matchup = {
---             -- 	enable = true,
---             -- },
---
---             -- https://github.com/nvim-treesitter/playground#query-linter
---             query_linter = {
---                 enable = true,
---                 use_virtual_text = true,
---                 lint_events = { "BufWrite", "CursorHold" },
---             },
---
---             playground = {
---                 enable = true,
---                 disable = {},
---                 updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
---                 persist_queries = true, -- Whether the query persists across vim sessions
---                 keybindings = {
---                     toggle_query_editor = "o",
---                     toggle_hl_groups = "i",
---                     toggle_injected_languages = "t",
---                     toggle_anonymous_nodes = "a",
---                     toggle_language_display = "I",
---                     focus_language = "f",
---                     unfocus_language = "F",
---                     update = "R",
---                     goto_node = "<cr>",
---                     show_help = "?",
---                 },
---             },
---         },
---         config = function(_, opts)
---             require("nvim-treesitter.configs").setup(opts)
---
---             -- MDX
---             vim.filetype.add({
---                 extension = {
---                     mdx = "mdx",
---                 },
---             })
---             vim.treesitter.language.register("markdown", "mdx")
---         end,
---     },
--- }
+return {
+    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                -- "astro",
+                "cmake",
+                "cpp",
+                "css",
+                -- "fish",
+                "gitignore",
+                "go",
+                "graphql",
+                "http",
+                "java",
+                "php",
+                "rust",
+                "scss",
+                "sql",
+                -- "svelte",
+                "bash",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "yaml",
+            },
+
+            -- matchup = {
+            -- 	enable = true,
+            -- },
+
+            -- https://github.com/nvim-treesitter/playground#query-linter
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { "BufWrite", "CursorHold" },
+            },
+
+            playground = {
+                enable = true,
+                disable = {},
+                updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                persist_queries = true, -- Whether the query persists across vim sessions
+                keybindings = {
+                    toggle_query_editor = "o",
+                    toggle_hl_groups = "i",
+                    toggle_injected_languages = "t",
+                    toggle_anonymous_nodes = "a",
+                    toggle_language_display = "I",
+                    focus_language = "f",
+                    unfocus_language = "F",
+                    update = "R",
+                    goto_node = "<cr>",
+                    show_help = "?",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+
+            -- MDX
+            vim.filetype.add({
+                extension = {
+                    mdx = "mdx",
+                },
+            })
+            vim.treesitter.language.register("markdown", "mdx")
+        end,
+    },
+
+    -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
+    -- would overwrite `ensure_installed` with the new value.
+    -- If you'd rather extend the default config, use the code below instead:
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            -- add tsx and treesitter
+            vim.list_extend(opts.ensure_installed, {
+                "tsx",
+                "typescript",
+            })
+        end,
+    },
+}
