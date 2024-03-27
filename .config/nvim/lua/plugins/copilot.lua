f726d11d8b2f8f1cc562c77ac22265e747e877dd
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-03-27 18:39:53 +0200

    setup copilot and copilot status bar

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,30 +1,34 @@
 -- ~/.config/nvim/lua/plugins/copilot.lua
 -- copilot configuration
 return {
-    "zbirenbaum/copilot.lua",
+    {
+        "zbirenbaum/copilot.lua",
 
-    opts = {
-        suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<leader><Tab>" } },
-        panel = {
-            enabled = true,
-            auto_refresh = false,
-            keymap = {
-                jump_prev = "[[",
-                jump_next = "]]",
-                accept = "<CR>",
-                refresh = "gr",
-                open = "<M-CR>",
+        opts = {
+            suggestion = { enabled = true, auto_trigger = true, keymap = { accept = "<leader><Tab>" } },
+            panel = {
+                enabled = true,
+                auto_refresh = false,
+                keymap = {
+                    jump_prev = "[[",
+                    jump_next = "]]",
+                    accept = "<CR>",
+                    refresh = "gr",
+                    open = "<M-CR>",
+                },
+                layout = {
+                    position = "bottom", -- | top | left | right
+                    ratio = 0.4,
+                },
             },
-            layout = {
-                position = "bottom", -- | top | left | right
-                ratio = 0.4,
+            filetypes = {
+                markdown = true,
+                help = true,
             },
-        },
-        filetypes = {
-            markdown = true,
-            help = true,
         },
     },
+
+    { "AndreM222/copilot-lualine" },
 }
 -- For additional configurations for the copilot panel, suggestions, filetypes supported, etc
 -- see https://github.com/zbirenbaum/copilot.lua
