5a89e77973676279252c4f8bddc33fc6f3ac343e
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-03-28 16:51:50 +0200

    add chatgpt feature in nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -37,7 +37,21 @@ require("lazy").setup({
             priority = 1000,
             opts = {},
         },
-
+        {
+            "jackMort/ChatGPT.nvim",
+            event = "VeryLazy",
+            config = function()
+                require("chatgpt").setup({
+                    api_key_cmd = "echo $OPENAI_API_KEY",
+                })
+            end,
+            dependencies = {
+                "MunifTanjim/nui.nvim",
+                "nvim-lua/plenary.nvim",
+                "folke/trouble.nvim",
+                "nvim-telescope/telescope.nvim",
+            },
+        },
         -- {
         --     "David-Kunz/gen.nvim",
         --     opts = {
