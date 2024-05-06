155c85ce41647ec73bae753776d34c1f140b3c64
AkaboxLinux <AkaboxLinux@aka.com>
2024-05-06 11:16:27 +0300

    add reenable spotlight script

--- misc/disablespotlight.sh
+++ misc/disablespotlight.sh
@@ -4,3 +4,6 @@ sudo mdutil -a -i off
 
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
 
+# to enable it back
+# sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+# sudo mdutil -a -i on
