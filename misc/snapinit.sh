12ab0c3b1781426db13897652d84cc17ef8bf7a6
AkaboxLinux <AkaboxLinux@aka.com>
2024-05-07 10:28:53 +0300

    install btop from local bin
    the local bin support gpu status

--- misc/snapinit.sh
+++ misc/snapinit.sh
@@ -4,4 +4,8 @@ sudo snap install --beta discord --classic
 sudo snap install --beta gtk-common-themes --classic
 sudo snap install --beta slack --classic
 sudo snap install --beta sublime-text --classic
-sudo snap install btop
+# sudo snap install btop
+#
+
+# install btop from local btopbin
+sudo cp btopbin/btop /usr/bin/btop
