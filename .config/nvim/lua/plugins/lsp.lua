436b70a028e7d0e2ef1e6e3c2f8dcc434243840f
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-05-10 00:05:07 +0300

    add python lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -18,6 +18,7 @@ return {
                 "tailwindcss-language-server",
                 "typescript-language-server",
                 "css-lsp",
+                "pyright",
             })
         end,
     },
