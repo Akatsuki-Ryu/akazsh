89fb70e4285de4ec40461e2b5739b65ac78db749
AkaCyberMac <akacybermac@aka.com>
2024-08-05 01:41:41 +0300

    update remote setup code

--- misc/rdp/remotedesktopsetup.sh
+++ misc/rdp/remotedesktopsetup.sh
@@ -2,4 +2,14 @@ sudo apt install xfce4 xfce4-goodies -y
 sudo apt install xrdp -y
 sudo systemctl start xrdp
 sudo adduser xrdp ssl-cert
-cp .xsessionrc ~/
+
+sudo rm -rf ~/.xsessionrc
+sudo touch ~/.xsessionrc
+
+
+# write .xsessionrc to ~/ as root user
+echo 'export GNOME_SHELL_SESSION_MODE=ubuntu' | sudo tee ~/.xsessionrc
+echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" | sudo tee ~/.xsessionrc
+
+
+sudo systemctl restart xrdp
