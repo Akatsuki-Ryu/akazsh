3a70cfac2a59cec7cdb5131b1a381cd1a2bf043e
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-08-05 16:27:59 +0300

    opt performance for the rdp

--- misc/rdp/remotedesktopsetup.sh
+++ misc/rdp/remotedesktopsetup.sh
@@ -4,12 +4,11 @@ sudo systemctl start xrdp
 sudo adduser xrdp ssl-cert
 
 sudo rm -rf ~/.xsessionrc
-sudo touch ~/.xsessionrc
-
+# sudo touch ~/.xsessionrc
 
 # write .xsessionrc to ~/ as root user
-echo 'export GNOME_SHELL_SESSION_MODE=ubuntu' | sudo tee ~/.xsessionrc
-echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" | sudo tee ~/.xsessionrc
-
+# echo 'export GNOME_SHELL_SESSION_MODE=ubuntu' | sudo tee ~/.xsessionrc
+# echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" | sudo tee ~/.xsessionrc
+# it turned out the performance of having this xsessionrc file is not good
 
 sudo systemctl restart xrdp
