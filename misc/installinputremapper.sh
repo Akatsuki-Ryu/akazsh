329ac829ebacf81fa78ad04ab03ada9b3c60bd3a
AkaboxTerminal <AkaboxTerminal@aka.com>
2024-08-20 01:09:40 +0300

    add input remapper install script for linux

---
+++ misc/installinputremapper.sh
@@ -0,0 +1,4 @@
+sudo apt install git python3-setuptools gettext
+git clone https://github.com/sezanzeb/input-remapper.git
+cd input-remapper && ./scripts/build.sh
+sudo apt install -f ./dist/input-remapper-2.0.1.deb
