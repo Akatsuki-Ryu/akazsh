c2636098dfd2e10e4b46688aa0fd4c2ac09c1710
AkaCyberMac <akacybermac@aka.com>
2024-11-13 00:41:20 +0200

    add dreset command

--- misc/dockercommands/dresetall.sh
+++ misc/dockercommands/dresetall.sh
@@ -5,9 +5,9 @@ docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 #remove all the volumes
-# docker volume rm $(docker volume ls -q)
+docker volume rm $(docker volume ls -q)
 #remove all the networks
-# docker network rm $(docker network ls -q)
+docker network rm $(docker network ls -q)
 #remove all the containers
 docker container prune -f
 #remove all the images
