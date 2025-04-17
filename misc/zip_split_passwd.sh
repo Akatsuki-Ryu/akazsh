ef0e57f03f733511c2280458d6be990c6794f43f
Akatsuki Ryu <akatsuki.liu@digialist.com>
2025-04-17 22:37:14 +0300

    update zippwdsp logic to include all the subfolders

--- misc/zip_split_passwd.sh
+++ misc/zip_split_passwd.sh
@@ -1,10 +1,17 @@
-echo input the file name
+#!/bin/bash
+
+# Get the current directory name as default filename
+default_filename=$(basename "$(pwd)")
+echo "File name (default: $default_filename):"
 read filename
-echo input the package password
+filename=${filename:-$default_filename}
+
+echo "Input the package password:"
 read password
-echo input the split size in mb
+echo "Input the split size in mb:"
 read volsize
-zip -P $password $filename+compress.zip *
+zip -r -P $password $filename+compress.zip *
 
 zip -r -s $volsize\m $filename.zip $filename+compress.zip
 rm -r $filename+compress.zip
+
