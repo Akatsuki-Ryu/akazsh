864310af56d1f340a5f92691aa1dd736bfc882e1
Akatsuki Ryu <akatsuki.liu@digialist.com>
2025-08-08 14:22:44 +0300

    add lt option

---
+++ misc/ezacommand.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Color codes
+green='\033[0;32m'
+reset='\033[0m'
+
+eza --long --tree -abghHS -s modified --icons --color always --color-scale "$@"
+
+echo
+
+folder_size=$(du -sh . | cut -f1)
+file_count_recursive=$(fd . --type f | wc -l)
+file_count_current=$(fd . --maxdepth 1 --type f | wc -l)
+
+label_width=50   # increased width for more space
+
+printf "%-${label_width}s %10b\n" "Current folder size (including subfolders):" "${green}${folder_size}${reset}"
+printf "%-${label_width}s %10b\n" "File count (recursive):" "${green}${file_count_recursive}${reset}"
+printf "%-${label_width}s %10b\n" "File count (current folder only):" "${green}${file_count_current}${reset}"
