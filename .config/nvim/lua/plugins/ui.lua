3b008c4106d71eeac82344cbdc5e6bb1b3daa1fe
Akatsuki Ryu <akatsuki.liu@digialist.com>
2025-09-17 15:21:42 +0300

    update nvim settings

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -160,7 +160,7 @@ return {
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
 
 
-      ]]
+]]
 
             logo = string.rep("\n", 8) .. logo .. "\n\n"
             opts.config.header = vim.split(logo, "\n")
