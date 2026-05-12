0f2bd63175190b1afa54b831dce1d93c6088e903
Akatsuki Ryu <akatsuki.liu@digialist.com>
2026-05-12 04:48:36 +0300

    fix nvim issues

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,12 +4,12 @@ vim.g.mapleader = " "
 vim.g.clipboard = {
   name = 'OSC 52',
   copy = {
-    ['+'] = require('vim.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.clipboard.osc52').copy('*'),
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
   },
   paste = {
-    ['+'] = require('vim.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.clipboard.osc52').paste('*'),
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
   },
 }
 
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,22 +26,23 @@ return {
     -- lsp servers
     {
         "neovim/nvim-lspconfig",
-        init = function()
-            local keys = require("lazyvim.plugins.lsp.keymaps").get()
-            keys[#keys + 1] = {
-                "gd",
-                function()
-                    -- DO NOT RESUSE WINDOW
-                    require("telescope.builtin").lsp_definitions({ reuse_win = false })
-                end,
-                desc = "Goto Definition",
-                has = "definition",
-            }
-        end,
         opts = {
             inlay_hints = { enabled = false },
             ---@type lspconfig.options
             servers = {
+                ["*"] = {
+                    keys = {
+                        {
+                            "gd",
+                            function()
+                                -- DO NOT REUSE WINDOW
+                                require("telescope.builtin").lsp_definitions({ reuse_win = false })
+                            end,
+                            desc = "Goto Definition",
+                            has = "definition",
+                        },
+                    },
+                },
                 cssls = {},
                 tailwindcss = {
                     root_dir = function(...)
